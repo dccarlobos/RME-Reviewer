ccedd54e01da834cc6b9f6f033493eeb2a58576b
dccarlobos <dccarlobos@gmail.com>
2026-01-27 21:06:49 +0800

    Delete plugins directory

--- plugins/admob-plus-cordova/www/ads/rewarded-interstitial.d.ts
+++
@@ -1,9 +0,0 @@
-import { MobileAd } from "./base";
-import type { RewardedAdOptions } from "./rewarded";
-export type RewardedInterstitialAdOptions = RewardedAdOptions;
-export declare class RewardedInterstitialAd extends MobileAd<RewardedInterstitialAdOptions> {
-    static readonly cls = "RewardedInterstitialAd";
-    isLoaded(): Promise<boolean>;
-    load(): Promise<void>;
-    show(): Promise<unknown>;
-}
