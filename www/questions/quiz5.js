54728d93ef92930a10b08878a90b70f1a4acd491
dccarlobos <dccarlobos@gmail.com>
2026-01-28 16:41:23 +0800

    Add files via upload

---
+++ www/questions/quiz5.js
@@ -0,0 +1,504 @@
+// 100 questions dito
+window.quizQuestions = [
+  {
+question:"1. What type of transformer is built into kWh meters to ensure that the voltage and current remain in quadrature?",
+options:["Quadrature transformer","Reactive transformer","Current transformer","Phase-shifting transformer"],
+answer:3
+},
+{
+question:"2. What do you call a complete, compact, and insulated switching equipment assembly?",
+options:["Switchboard","Panel board","Switchgear","Switchyard"],
+answer:2
+},
+{
+question:"3. Which device provides both protection against power reversal and overload simultaneously?",
+options:["Distance relay","Directional overcurrent relay","Differential relay","Phase unbalance relay"],
+answer:1
+},
+{
+question:"4. Grounding rods used in substations should be buried at least how many feet below the ground?",
+options:["5","6","7","8"],
+answer:3
+},
+{
+question:"5. The selection of support structures for transmission line construction depends on several factors, except:",
+options:["Terrain to be crossed","Size of conductors","Economy and atmospheric conditions","Equipment to be carried"],
+answer:2
+},
+{
+question:"6. What do you call the process of cutting a slightly concave recess on a pole (about ½ inch deep) to hold the cross arm firmly in place?",
+options:["Pole shaving","Gaining","Roofing","Slabbing"],
+answer:1
+},
+{
+question:"7. In pin-type insulators, the conductor is tied to the top or side groove using what wire?",
+options:["Messenger wire","Loop wire","Pin wire","Tie wire"],
+answer:3
+},
+{
+question:"8. In suspension-type insulators, conductors are secured in place using a clamp or what device?",
+options:["Dip","Bracket","Shoe","Tie wire"],
+answer:2
+},
+{
+question:"9. When energized conductors must be handled safely without direct contact, they are managed using insulated tools called:",
+options:["Insulated sticks","Hot sticks","Covered sticks","Rubberized sticks"],
+answer:1
+},
+{
+question:"10. What do you call the insulating component that lines the hole in a transformer tank through which the conductor passes?",
+options:["Bushing","Shoe","Busway","Raceway"],
+answer:0
+},
+{
+question:"11. Which among the following is NOT a type of transformer bushing?",
+options:["Inductor-type bushing","Solid porcelain bushing","Oil-filled bushing","Capacitor-type bushing"],
+answer:0
+},
+{
+question:"12. A type of distribution transformer known as CSP stands for:",
+options:["Conventional-self-protected","Controlled-self-protected","Completely-self-protected","Constrained-self-protected"],
+answer:2
+},
+{
+question:"13. This device isolates a section of a circuit while keeping the rest energized.",
+options:["Fuse link","Fuse remover","Fuse cut-out","Fuse separator"],
+answer:2
+},
+{
+question:"14. Which of the following is NOT a type of armature winding?",
+options:["Lap","Ring","Wave","Progressive"],
+answer:3
+},
+{
+question:"15. What kind of ammeter measures current without disconnecting the circuit?",
+options:["Wireless ammeter","Dynamometer","Clamp ammeter","Hook-out ammeter"],
+answer:2
+},
+{
+question:"16. Which instrument is used to test insulation in high-voltage systems?",
+options:["Megger","Hi-pot tester","Ohmmeter","Ducter"],
+answer:1
+},
+{
+question:"17. What device is used to measure the contact resistance of circuit breakers?",
+options:["Multimeter","Breaker meter","Ohmmeter","Ducter"],
+answer:3
+},
+{
+question:"18. What type of clamp holds securely under normal tension but slips when the conductor tension reaches about half of its maximum in case of a break?",
+options:["Suspension clamp","Sliding clamp","Trunnion-type clamp","Releasing clamp"],
+answer:3
+},
+{
+question:"19. What type of clamp provides flexible connection by supporting the clamp on a pivot aligned with the conductor’s axis, allowing vibration to transfer freely?",
+options:["Dead-end clamp","Slip clamp","Trunnion clamp","Releasing clamp"],
+answer:2
+},
+{
+question:"20. What is the unit of electrical resistance?",
+options:["Ohm","Volt","Ampere","Watt"],
+answer:0
+},
+{
+question:"21. As the length of a conductor increases, its resistance will:",
+options:["Decrease","Increase","Remain the same","None of these"],
+answer:1
+},
+{
+question:"22. As the cross-sectional area of a conductor increases, its resistance will:",
+options:["Decrease","Increase","Remain the same","None of these"],
+answer:0
+},
+{
+question:"23. When the temperature of copper increases, its resistance will:",
+options:["Decrease","Increase","Remain the same","None of these"],
+answer:1
+},
+{
+question:"24. When the temperature of carbon increases, its resistance will:",
+options:["Decrease","Increase","Remain the same","None of these"],
+answer:0
+},
+{
+question:"25. The specific resistance of a conductor depends on its:",
+options:["Material composition","Length","Cross-sectional area","Total resistance"],
+answer:0
+},
+{
+question:"26. Which of the following insulating materials is most affected by heat?",
+options:["PVC","Mica","Paper","Leatheroid"],
+answer:0
+},
+{
+question:"27. As temperature rises, the insulating property of an insulator tends to:",
+options:["Weaken","Improve","Remain unchanged","None of these"],
+answer:0
+},
+{
+question:"28. The specific resistance of mica is:",
+options:["Very high","Zero","Very low","Low"],
+answer:2
+},
+{
+question:"29. As temperature increases, the temperature coefficient of resistance generally:",
+options:["Increases","Remains constant","Becomes zero","Decreases"],
+answer:3
+},
+{
+question:"30. Which of the following materials has the lowest specific resistance?",
+options:["Copper","Silver","Aluminum","Iron"],
+answer:1
+},
+{
+question:"31. Which of the following materials has a negative temperature coefficient of resistance?",
+options:["Aluminum","Iron","Carbon","Nichrome"],
+answer:2
+},
+{
+question:"32. Which of the following materials has the lowest resistivity?",
+options:["Copper","Aluminum","Iron","Lead"],
+answer:0
+},
+{
+question:"33. Which material has nearly zero temperature coefficient of resistance?",
+options:["Silver","Aluminum","Manganin","Copper"],
+answer:2
+},
+{
+question:"34. What is the standard unit of electric charge?",
+options:["Volt-ampere (VA)","Farad (F)","Henry (H)","Coulomb (C)"],
+answer:3
+},
+{
+question:"35. Static electricity is produced by:",
+options:["Chemical reaction","Friction","Induction","Both friction and induction"],
+answer:1
+},
+{
+question:"36. A paper-type capacitor is classified as a:",
+options:["Variable capacitor","Electrolytic capacitor","Fixed capacitor","None of these"],
+answer:2
+},
+{
+question:"37. Mica or ceramic capacitors are examples of:",
+options:["Fixed capacitors","Variable capacitors","Electrolytic capacitors","None of these"],
+answer:0
+},
+{
+question:"38. If the capacitor plates have loose fittings, its equivalent series resistance will:",
+options:["Increase","Decrease","Become extremely high","Become extremely low"],
+answer:0
+},
+{
+question:"39. The practical unit used to measure the capacitance of a capacitor is:",
+options:["Erg","Joule","Coulomb","Farad"],
+answer:3
+},
+{
+question:"40. In a single-phase AC motor, the capacitor is used to:",
+options:["Split the phase","Reduce radio interference","Reduce losses","Reduce current"],
+answer:0
+},
+{
+question:"41. In a fluorescent lamp, the capacitor is used to:",
+options:["Split the phase","Reduce the time for light emission","Minimize radio interference","Reduce circuit current"],
+answer:2
+},
+{
+question:"42. In an AC inductive circuit, a capacitor is used to:",
+options:["Decrease the power factor","Improve the power factor","Keep the power factor constant","None of these"],
+answer:1
+},
+{
+question:"43. A capacitor bank installed across a factory load is used to:",
+options:["Improve the power factor","Reduce the power factor","Improve voltage stability","Provide quick motor starting"],
+answer:0
+},
+{
+question:"44. The function of the depolarizer in a carbon-zinc cell is to:",
+options:["Convert the produced hydrogen into water","Prevent rapid chemical action on the zinc container","Recombine the decomposed electrolyte","Absorb oxygen generated within the cell"],
+answer:0
+},
+{
+question:"45. The density of the electrolyte in a lead-acid battery indicates the:",
+options:["Possible damage to the plates","Level of the electrolyte","Battery’s electromotive force (EMF)","State of charge of the battery"],
+answer:3
+},
+{
+question:"46. What safety precaution must be observed during battery charging to prevent accidents?",
+options:["Explosive oxy-hydrogen gas is formed during overcharging","Poisonous chlorine gas is produced during charging","Nitrous oxides are generated during overcharging","Overheating causes the acid to boil and spill out"],
+answer:0
+},
+{
+question:"47. Which of the following serves as a depolarizer in a dry cell?",
+options:["Carbon powder","Manganese dioxide","Ammonium chloride","Zinc chloride"],
+answer:1
+},
+{
+question:"48. One advantage of an iron-nickel battery compared to a lead-acid battery is that it:",
+options:["Has a higher cell voltage","Has greater efficiency","Requires less maintenance","Is much cheaper"],
+answer:2
+},
+{
+question:"49. When charging batteries, one should always:",
+options:["Connect and disconnect them while the supply is on","Keep them in a well-ventilated area","Check the acid level using a lighted match","Short the cells to test if fully charged"],
+answer:1
+},
+{
+question:"50. When a dielectric slab is inserted between the plates of a parallel-plate capacitor, the potential difference between the plates will:",
+options:["Decrease","Remain the same","Increase","Become zero"],
+answer:0
+},
+{
+question:"51. With which type of capacitor must the correct polarity be observed during connection?",
+options:["Paper capacitor","Mica capacitor","Ceramic capacitor","Electrolytic capacitor"],
+answer:3
+},
+{
+question:"52. Ceiling fan motors typically use a 2.3 microfarad capacitor. This capacitor is usually of the type:",
+options:["Paper capacitor","Electrolytic capacitor","Parallel-plate with mica dielectric","None of these"],
+answer:0
+},
+{
+question:"53. The counter electromotive force (EMF) in a motor is zero when:",
+options:["The motor runs at rated speed","The motor is near rated speed","The armature is stationary","The armature just begins to rotate"],
+answer:2
+},
+{
+question:"54. In a generator, the interpole always has the same polarity as the:",
+options:["Pole that follows it","Pole that precedes it","Opposite main pole","None of the above"],
+answer:1
+},
+{
+question:"55. When the field resistance of a shunt motor is increased, its speed will:",
+options:["Decrease","Remain the same","Increase","None of the above"],
+answer:2
+},
+{
+question:"56. The main characteristic of a shunt motor is that it:",
+options:["Has high starting torque","Is unaffected by load at high speed","Maintains nearly constant speed under varying load","Does not slow down when overloaded"],
+answer:2
+},
+{
+question:"57. The direction of rotation of a split-phase induction motor can be reversed by:",
+options:["Reversing the starting winding connections","Reversing the main winding connections","Reversing one phase lead","Reversing any two leads"],
+answer:0
+},
+{
+question:"58. A synchronous converter is used to:",
+options:["Convert AC to DC","Convert DC to AC","Change synchronous to induction operation","Change induction to synchronous operation"],
+answer:0
+},
+{
+question:"59. If a motor must operate slightly overloaded for a short period, you should:",
+options:["Install larger fuses","Monitor bearing and motor temperatures closely","Notify the chief engineer","Bypass the overload relay"],
+answer:1
+},
+{
+question:"60. Before disconnecting an AC generator that is operating in parallel with others, you should first:",
+options:["Reduce the voltage","Reduce field excitation","Reduce the steam input using the governor mechanism","Close the throttle"],
+answer:2
+},
+{
+question:"61. The main advantage of DC motors over AC motors is that they:",
+options:["Can operate at higher speeds","Can lift heavier loads","Provide better speed control over a wide range","Have a longer service life"],
+answer:2
+},
+{
+question:"62. Motor leads are insulated by:",
+options:["Applying heat-resistant enamel","Applying heat-resistant varnish","Using heat-resistant white lead","Using heat-resistant red lead"],
+answer:1
+},
+{
+question:"63. Which of the following will NOT cause a generator to vibrate?",
+options:["Misalignment","Loose bolts","Loose pigtails","Defective speed governor"],
+answer:2
+},
+{
+question:"64. Which of the following can cause generator brushes to burn?",
+options:["Brushes not set in the neutral plane","Generator overload","Weak brush springs","All of the above"],
+answer:3
+},
+{
+question:"65. A universal motor must be a:",
+options:["Series-wound motor","Shunt-wound motor","Compound-wound motor","Either A or C"],
+answer:0
+},
+{
+question:"66. When should an electric hand tool be grounded?",
+options:["When it operates on DC power","When it operates on AC power","When operating any tool over 100 volts","Only when working on wet steel plates"],
+answer:2
+},
+{
+question:"67. Which of the following conditions would cause sparking at the brushes?",
+options:["High mica","Dirty commutator","Grooved commutator","Any of the above"],
+answer:3
+},
+{
+question:"68. The safest solvent for cleaning electric motors is:",
+options:["Carbon tetrachloride","Diluted muriatic acid","Carbon dioxide","Trichloroethylene"],
+answer:3
+},
+{
+question:"69. Which of the following conditions can cause a bearing to overheat?",
+options:["Poor alignment","Overloading","Bearings fitted too tightly","All of the above"],
+answer:3
+},
+{
+question:"70. Which of the following will NOT cause a motor bearing to overheat?",
+options:["Insufficient lubrication","Loose brushes","Overloading","All of the above"],
+answer:1
+},
+{
+question:"71. An open armature coil can be identified by:",
+options:["An increase in motor speed","A decrease in motor speed","Sparking at the brushes","A ring of fire appearing around the commutator"],
+answer:3
+},
+{
+question:"72. Most generators can safely withstand an overload of:",
+options:["30%","25%","50%","35%"],
+answer:1
+},
+{
+question:"73. Which of the following conditions can prevent a generator from building up voltage?",
+options:["Reversed field connections","Speed too low","Machine short-circuited","Any of the above"],
+answer:3
+},
+{
+question:"74. Interpoles in a DC machine are connected:",
+options:["In series with the armature","In parallel with the armature","In parallel with the series field","In series with the shunt field"],
+answer:0
+},
+{
+question:"75. If an electric motor catches fire, the first thing to do is:",
+options:["Use a CO₂ fire extinguisher","Use a dry chemical extinguisher","Shut down or disconnect the motor","Slow the motor down until it cools off"],
+answer:2
+},
+{
+question:"76. Armature cores are laminated to:",
+options:["Increase magnetic flux lines","Increase magnetic strength","Reduce eddy current losses","Reduce heat losses"],
+answer:2
+},
+{
+question:"77. Which of the following is NOT a good conductor of electricity?",
+options:["Copper","Mica","Silver","Aluminum"],
+answer:1
+},
+{
+question:"78. A synchronous motor is excited using:",
+options:["A synchronizer","AC current","DC current","Three-phase current"],
+answer:2
+},
+{
+question:"79. A universal series motor can operate on:",
+options:["DC power only","Three-phase AC only","Either AC or DC power","Single-phase AC only"],
+answer:2
+},
+{
+question:"80. One key advantage of DC motors over AC motors is that they:",
+options:["Require less maintenance","Provide better speed control","Are less expensive","Can be started directly across the line"],
+answer:1
+},
+{
+question:"81. The main purpose of an exciting current is to",
+options:["energize a dead circuit","produce a magnetic field","start a synchronous motor","charge a battery"],
+answer:1
+},
+{
+question:"82. Moisture in armature windings can result in",
+options:["low voltage output","low current","overheating of the winding","increased resistance"],
+answer:2
+},
+{
+question:"83. If one segment of a commutator turns black, the usual cause is",
+options:["a grounded coil","a shorted coil","an open coil","any of these issues"],
+answer:0
+},
+{
+question:"84. The speed of a DC shunt motor increases when",
+options:["the brushes are shifted","field resistance is increased","field resistance is decreased","none of these"],
+answer:2
+},
+{
+question:"85. If the field rheostat of a DC generator is far from its normal setting, a likely cause is",
+options:["prime mover not running at proper speed","voltage too high","excessive load","an open field coil"],
+answer:0
+},
+{
+question:"86. Reversing the polarity of a DC line will make connected motors",
+options:["stop running","speed up","reverse rotation","run in the same direction"],
+answer:3
+},
+{
+question:"87. When two alternators in parallel have unequal loads, you should",
+options:["adjust the frequency","change the prime mover speed","reduce field excitation","increase field excitation"],
+answer:1
+},
+{
+question:"88. The term SELSYN comes from",
+options:["the inventor’s name","a brand name","the abbreviation of self-synchronous","none of these"],
+answer:2
+},
+{
+question:"89. In an AC motor, the part that functions like the armature of a DC motor is the",
+options:["field coils","stator","rotor","armature"],
+answer:1
+},
+{
+question:"90. In an AC motor, the part that functions like the field of a DC motor is the",
+options:["stator","field coil","rotor","armature"],
+answer:2
+},
+{
+question:"91. What type of winding is installed on reciprocating-driven generators to smooth out speed variations?",
+options:["compound winding","amortisseur winding","shunt winding","none, due to flywheel action"],
+answer:1
+},
+{
+question:"92. Series motors usually transmit power to their load through",
+options:["chains","belts","pulleys","direct coupling"],
+answer:3
+},
+{
+question:"93. In compound DC generators, the interpoles are connected in",
+options:["series with the series field","series with the armature","series with the shunt field","series with both series and shunt fields"],
+answer:1
+},
+{
+question:"94. To test the overspeed trip of a generator, the correct procedure is to",
+options:["apply load until it trips","avoid touching unless in emergency","bring to full load then reset after trip","gradually raise speed until it trips, then reset while running"],
+answer:3
+},
+{
+question:"95. Excessive mica between commutator segments is removed by",
+options:["filing","burning","undercutting","chiseling"],
+answer:2
+},
+{
+question:"96. New motor brushes are properly seated by",
+options:["pulling sandpaper between brush and commutator","using crocus cloth","using emery cloth","filing the brush face"],
+answer:0
+},
+{
+question:"97. The commutator of a DC machine is made of",
+options:["copper segments","bronze segments","nickel segments","steel segments"],
+answer:0
+},
+{
+question:"98. Brushes are staggered mainly to",
+options:["balance the load","avoid uneven commutator wear","reduce vibration","cover more area for higher current"],
+answer:1
+},
+{
+question:"99. Circular grooves on a commutator surface indicate",
+options:["wrong brush grade","high mica level","incorrect brush alignment","open armature circuit"],
+answer:2
+},
+{
+question:"100. Sparking and grooving on a commutator may be caused by",
+options:["overloading","incorrect connections","wrong brush type","any of these factors"],
+answer:3
+},
+  // ... hanggang 100 questions
+];
